dc0fbc690f073a9cfaf9d5900e88cd072793119b
MDiao8 <diaomo32@gmail.com>
2026-08-20 00:00:10 +0000

    Update project modifications

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const { randomUUID } = require('crypto');
 const { corsConfig } = require('./config/security');
 const logger = require('./config/logger');
 const { globalRateLimit } = require('./middlewares/rateLimit.middleware');
-const errorMiddleware = require('./middlewares/error.middleware');
+const { errorMiddleware } = require('./middlewares/error.middleware');
 
 const authRoutes = require('./routes/auth.route');
 const clientColisRoutes = require('./routes/client/colis.route');
